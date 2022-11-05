765ce1960d53bd17d0a6b14e3fe5f8cafccb69c9
kolesnikdi <kolesnik.d.i@gmail.com>
2022-11-05 17:29:49 +0200

    [pytest] make tests for blog and registration

---
+++ blog/tests/tests_blog.py
@@ -0,0 +1,94 @@
+import pytest
+
+from django.urls import reverse
+
+from rest_framework import status, exceptions
+
+from blog.serializers import SnippetSerializer
+
+
+class TestSnippetViewSet:
+
+    @pytest.mark.django_db
+    def test_authenticated(self, api_client):
+        url = reverse('rest_framework:login')
+        data = {
+            'username': 'DjangoBoy2',
+            'password': '122122saassaass',
+        }
+        response = api_client.post(url, data=data, format='json')
+        assert response.status_code == status.HTTP_200_OK
+
+    @pytest.mark.django_db
+    def test_post_not_authenticated(self, api_client, randomizer):
+        url = reverse('post-list')
+        data = {
+            'title': randomizer.random_name(),
+            'text': randomizer.upp2_data(),
+        }
+        response = api_client.post(url, data=data, format='json')
+        assert response.json()
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    @pytest.mark.django_db
+    def test_post_valid_data(self, authenticated_client, randomizer):
+        url = reverse('post-list')
+        data = {
+            'title': randomizer.random_name(),
+            'text': randomizer.upp2_data(),
+        }
+        response = authenticated_client.post(url, data=data, format='json')
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.json()
+        assert set(response.json().keys()) == set(SnippetSerializer.Meta.fields)
+        assert response.json()['title'] == data['title']
+        assert response.json()['text'] == data['text']
+
+    @pytest.mark.django_db
+    def test_post_invalid_data(self, authenticated_client, randomizer):
+        url = reverse('post-list')
+        data = {
+            'title': None,
+            'text': None,
+        }
+        response = authenticated_client.post(url, data=data, format='json')
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.json()
+        assert response.json()['title'] == ['This field may not be null.']
+        assert response.json()['text'] == ['This field may not be null.']
+
+    @pytest.mark.django_db
+    def test_post_valid_data_change(self, api_client, created_blog, randomizer):
+        url = reverse('post-detail', kwargs={'pk': created_blog.id})
+        data = {
+            'title': randomizer.random_name(),
+            'text': randomizer.upp2_data(),
+        }
+        response = api_client.put(url, data=data, format='json')
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json()
+        assert set(response.json().keys()) == set(SnippetSerializer.Meta.fields)
+        assert response.json()['title'] != created_blog.title
+        assert response.json()['text'] != created_blog.text
+
+    @pytest.mark.django_db
+    def test_post_valid_data_change_not_owner(self, authenticated_client, created_blog, randomizer):  # todo maybe pytest don`t see permisions?
+        url = reverse('post-detail', kwargs={'pk': created_blog.id})
+        data = {
+            'title': randomizer.random_name(),
+            'text': randomizer.upp2_data(),
+        }
+        response = authenticated_client.put(url, data=data, format='json')
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    @pytest.mark.django_db
+    def test_post_valid_data_delete(self, api_client, created_blog, randomizer):
+        url = reverse('post-detail', kwargs={'pk': created_blog.id})
+        response = api_client.delete(url, format='json')
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    @pytest.mark.django_db
+    def test_post_valid_data_delete_not_owner(self, authenticated_client, created_blog):  # todo maybe pytest don`t see permisions?
+        url = reverse('post-detail', kwargs={'pk': created_blog.id})
+        response = authenticated_client.delete(url, format='json')
+        assert response.status_code == status.HTTP_403_FORBIDDEN
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
+from django.utils import timezone
+
 from rest_framework import permissions, renderers, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -12,10 +14,11 @@ class SnippetViewSet(viewsets.ModelViewSet):
     serializer_class = SnippetSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
+
     @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
-    def highlight(self, request, *args, **kwargs):
+    def highlight(self, request, *args, **kwargs):  # open blog text in new link
         blog_text = self.get_object()
         return Response(blog_text.text)
 
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+    def perform_create(self, serializer):   # signs the post by name of user
+        serializer.save(author=self.request.user, published_date=timezone.now())
